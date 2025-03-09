a104ed91d56224097da7e4cab3285d22f98be8aa
diogo <diogocruzdc94@gmail.com>
2025-03-09 08:45:20 -0300

    homesreen updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
-import { Text, View,Image, ScrollView } from "react-native";
+import { Text, View,Image, ScrollView, ActivityIndicator, FlatList } from "react-native";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/searchBar";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { FetchMovies } from "@/services/api";
+import MovieCard from "@/components/movieCard";
+
 
 export default function Index() {
   const router = useRouter();
 
+  const {
+    data:movies, 
+    loading:moviesLoading,
+  error:moviesError 
+}= useFetch(()=> FetchMovies({
+    query: "",
+  }))
 
   return (
     <View className='flex-1 bg-primary' >
@@ -17,13 +28,46 @@ export default function Index() {
 
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        <View className="flex-1 mt-5">
-          <SearchBar
-          onPress={() => router.push("/search")}
-          placeholder="Procure por um filme"
-          />
+      {moviesLoading ? (<ActivityIndicator 
+                      size="large" 
+                      color="#0000ff" 
+                      className="mt-10 self-center"
+                      />) : moviesError? (<Text>Erro ao buscar filmes</Text>) : 
+                      (<View className="flex-1 mt-5">
+                        <SearchBar
+                        onPress={() => router.push("/search")}
+                        placeholder="Procure por um filme"
+                        />
 
-        </View>
+                        <>
+                        <Text className=" text-lg font-bold mt-5 text-white mb-3">Ultimos lançamentos</Text>
+
+                          <FlatList
+                          data={movies}
+                          renderItem={({item}) => (
+                            <MovieCard 
+                            {...item}
+                            />
+                          )} 
+                          keyExtractor={(item) => item.id.toString()}
+                          numColumns={3}
+                          columnWrapperStyle={{
+                            justifyContent: "flex-start",
+                            gap: 20,
+                            paddingRight: 5,
+                            marginBottom: 10,
+                          }}
+
+                          className="mt-2 pb-32"
+                          scrollEnabled={false}
+                          />
+
+
+                        </>
+              
+                      </View>)}
+
+        
         </ScrollView>
 
     
--- services/api.ts
+++ services/api.ts
@@ -8,7 +8,7 @@ headers: {
 
 }
 
-export const FetchPopularMovies = async ({query}:{query:string}) => {
+export const FetchMovies = async ({query}:{query:string}) => {
     const endpoint = query
     ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` 
     : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
